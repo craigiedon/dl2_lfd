fa59282a80461f786a00024c8177134c51681456
craigiedon <craiginnes@gmail.com>
2019-07-09 14:17:06 +0100

    Made logging incremental, rather than just at the end

--- cnn_control.py
+++ cnn_control.py
@@ -1,14 +1,13 @@
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
 from torch import nn, optim
-from load_data import load_demos, save_nums, load_pose_demos
+from load_data import load_demos, save_num_append, show_torched_im
 from helper_funcs.rm import RobotModel
 
 from torchvision.transforms import Compose
 from helper_funcs.transforms import Crop, Resize
 
-from model import setup_model, load_model, setup_joints_model
+from model import setup_model, setup_joints_model
 
 
 import os
@@ -35,21 +34,23 @@ def loss_epoch(model, loss_func, d_loader, optimizer=None):
     return sum(losses) / len(losses)
 
 
-def train(model, train_loader, validation_loader, epochs, save_path=None):
+def train(model, train_loader, validation_loader, epochs, save_path):
 
-    optimizer = optim.Adam(model.parameters(), eps=1e-7)
+    optimizer = optim.Adam(model.parameters())
+    #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
     loss_criterion = nn.MSELoss()
 
     print("Beginning Training")
     print("Epoch\tTrain\tValidation")
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     
-    avg_losses_train = []
-    avg_losses_validation = []
     for epoch in range(epochs):
         # Training
         model.train()
         avg_loss_train = loss_epoch(model, loss_criterion, train_loader, optimizer)
-        avg_losses_train.append(avg_loss_train)
+        save_num_append(avg_loss_train, join(save_path, "train.txt"))
 
         temp_print("{}\t{}\t-".format(epoch, avg_loss_train))
 
@@ -57,20 +58,12 @@ def train(model, train_loader, validation_loader, epochs, save_path=None):
         model.eval()
         with torch.no_grad():
             avg_loss_validation = loss_epoch(model, loss_criterion, validation_loader)
-            avg_losses_validation.append(avg_loss_validation)
+            save_num_append(avg_loss_validation, join(save_path, "validation.txt"))
 
         print('\x1b[2K\r'),
         print("{}\t{}\t{}".format(epoch, avg_loss_train, avg_loss_validation))
 
-
-    if save_path is not None:
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-
-        save_nums(avg_losses_train, join(save_path, "train.txt"))
-        save_nums(avg_losses_validation, join(save_path, "validation.txt"))
-        torch.save(full_model.state_dict(), join(save_path, "e2e_control_full.pt"))
-    
+    torch.save(full_model.state_dict(), join(save_path, "e2e_control_full.pt"))
     print("Finished Training")
     return model
 
@@ -79,35 +72,30 @@ def train(model, train_loader, validation_loader, epochs, save_path=None):
 arm_joint_names = np.genfromtxt("config/arm_joint_names.txt", np.str)
 
 K_kinect = np.array([366.096588, 0 , 268, 0, 366.096588, 192,0,0,1]).reshape(3,3)
+"""
 robot_model = RobotModel(urdf_path="config/pr2.xml",
                          base_frame='base_link',
                          ee_frame='r_gripper_tool_frame',
                          camera_model=K_kinect)
+"""
 
 batch_size = 32
-num_epochs = 200
+num_epochs = 500
 image_glob = "kinect2_qhd_image_color_rect_*.jpg"
-im_height = 224
-im_width = 224
-im_trans = Compose([Crop(150, 475, 50, 800), Resize(224, 224)]) # TODO change to be configurable
+im_height = 240
+im_width = 240
+crop_top, crop_bottom, crop_left, crop_right = (150, 475, 50, 800)
+im_trans = Compose([Crop(crop_top, crop_bottom, crop_left, crop_right), Resize(im_height, im_width)]) # TODO change to be configurable
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 train_set, train_loader = load_demos("./demos/reach_blue_cube", image_glob, batch_size, arm_joint_names, im_trans, True, device, from_demo=0, to_demo=80)
-validation_set, validation_loader = load_demos("./demos/reach_blue_cube", image_glob, batch_size, arm_joint_names, im_trans, False, device, from_demo=80)
+validation_set, validation_loader = load_demos("./demos/reach_blue_cube", image_glob, batch_size, arm_joint_names, im_trans, False, device, from_demo=80, to_demo=81)
 
 
-"""
-# TODO: Move this into an easy-to-use function
-# im_canvas = img_heatmap_ax.imshow(raw_ims[0])
-processed_im = train_set[0][0][0].permute(1,2,0)
-print(processed_im.shape)
-plt.imshow(processed_im)
-plt.show()
-"""
 
 # Train the model
-exp_name = "BlueCubeSmallNet"
+exp_name = "BlueCubeLevineNet"
 log_path = "./logs/{}-{}".format(exp_name, t_stamp())
 full_model = setup_model(device, im_height, im_width, arm_joint_names)
 full_model = train(full_model, train_loader, validation_loader, num_epochs, log_path)
