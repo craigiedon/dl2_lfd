237071637464575ba4a4dd72e2af0fc3341a5a10
craigiedon <craiginnes@gmail.com>
2019-07-09 14:17:52 +0100

    Added zip chunks function

--- helper_funcs/utils.py
+++ helper_funcs/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import sys
+import torch
 
 def find_last(pred, lst):
     return next(x for x in reversed(lst) if pred(x))
@@ -11,3 +12,7 @@ def t_stamp():
 def temp_print(s):
     print (s + '\r'),
     sys.stdout.flush()
+
+
+def zip_chunks(tensor, num_chunks, dim=0):
+    return torch.stack(torch.chunk(tensor, num_chunks, dim), dim).transpose(dim,dim+1)
