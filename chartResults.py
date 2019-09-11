Changed from quaternions to radians, fixed a bug in forward model, and altered cost weightings

chartResults.py
@@ -208,11 +208,11 @@ def chart_pred_pose(model_path, demo_path, demo_num):
 
     with torch.no_grad():
         for ins in demo_loader:
-            rgb_ins, depth_ins, past_ins, current_ins, target_ins = ins 
-            next_pred, current_pred = model(rgb_ins, depth_ins, past_ins, current_ins)
+            rgb_ins, depth_ins, past_ins, target_ins = ins 
+            next_pred, current_pred = model(rgb_ins, depth_ins, past_ins)
             next_pred_all.append(next_pred)
             current_pred_all.append(current_pred)
-            current_targets_all.append(current_ins)
+            current_targets_all.append(past_ins[:, 4])
             next_targets_all.append(target_ins)
     
     next_pred_all = torch.cat(next_pred_all)
@@ -229,7 +229,7 @@ def chart_pred_pose(model_path, demo_path, demo_num):
 
 
     n_pose_dims, n_samples = next_pred_all.shape[0], next_pred_all.shape[1]
-    dim_names = ["p-x", "p-y", "p-z", "rot-x", "rot-y", "rot-z", "rot-w"]
+    dim_names = ["p-x", "p-y", "p-z", "roll", "pitch", "yaw"]
     for dim_id in range(n_pose_dims):
         plt.subplot(ceil(n_pose_dims / 3.0), 3, dim_id + 1)
         plt.plot(next_pred_all[dim_id], label="Predicted Pose")

imageAndDepth.py
@@ -6,7 +6,7 @@ import math
 
 from model import ZhangNet
 
-from helper_funcs.utils import load_json, temp_print, t_stamp
+from helper_funcs.utils import load_json, t_stamp
 from helper_funcs.transforms import get_trans, get_grayscale_trans
 from load_data import load_demos, show_torched_im, nn_input_to_imshow, append_tensors_as_csv
 from chartResults import plot_csv
@@ -22,12 +22,12 @@ def ZhangLoss():
         l2_loss = F.mse_loss(next_pose_pred, target_pred)
         l1_loss = F.l1_loss(next_pose_pred, target_pred)
 
-        eps = 1e-4
-        cos_sims = torch.clamp(F.cosine_similarity(next_pose_pred[:, 3:], target_pred[:, 3:]), -1 + eps, 1 - eps)
+        eps = 1e-7
+        cos_sims = torch.clamp(F.cosine_similarity(next_pose_pred, target_pred), -1 + eps, 1 - eps)
         angle_loss = torch.acos(cos_sims).mean()
 
         aux_loss = F.mse_loss(aux_pred, target_aux)
-        full_loss = 1e-2 * l2_loss + 1.0 * l1_loss + 5e-3 * angle_loss + 1e-4 * aux_loss
+        full_loss = 1e-2 * l2_loss + 1.0 * l1_loss + 5e-3 * angle_loss + 1e-2 * aux_loss
         if math.isnan(full_loss):
             print("Nan Encountered")
 
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         True,
         torch.device("cuda"),
         from_demo=0,
-        to_demo=60)
+        to_demo=90)
 
     val_set, validation_loader = load_rgbd_demos(
         exp_config["demo_folder"],
@@ -60,12 +60,11 @@ if __name__ == "__main__":
         get_grayscale_trans(im_params),
         False,
         torch.device("cuda"),
-        from_demo=60,
-        to_demo=80)
+        from_demo=90)
 
     model = ZhangNet(im_params["resize_height"], im_params["resize_width"])
     model.to(torch.device("cuda"))
-    optimizer = optim.Adam(model.parameters(), weight_decay=1e-4)
+    optimizer = optim.Adam(model.parameters(),weight_decay=1e-4)
     loss_criterion = ZhangLoss()
 
     results_folder = "logs/zhang-{}".format(t_stamp())
@@ -77,10 +76,10 @@ if __name__ == "__main__":
         train_losses = []
         with autograd.detect_anomaly():
             for i, in_batch in enumerate(train_loader):
-                temp_print("T Batch {}/{}".format(i, len(train_loader)))
-                rgb_ins, depth_ins, past_ins, current_ins, target_ins = in_batch 
-                next_pred, aux_pred = model(rgb_ins, depth_ins, past_ins, current_ins)
-                train_loss = loss_criterion(next_pred, aux_pred, target_ins, current_ins)
+                print("T Batch {}/{}".format(i, len(train_loader)), end='\r', flush=True)
+                rgb_ins, depth_ins, past_ins, target_ins = in_batch 
+                next_pred, aux_pred = model(rgb_ins, depth_ins, past_ins)
+                train_loss = loss_criterion(next_pred, aux_pred, target_ins, past_ins[:, 4])
                 train_losses.append([t.item() for t in train_loss])
                 optimizer.zero_grad()
                 train_loss[0].backward()
@@ -89,11 +88,11 @@ if __name__ == "__main__":
         model.eval()
         val_losses = []
         for i, in_batch in enumerate(validation_loader):
-            temp_print("V Batch {}/{}".format(i, len(validation_loader)))
+            print("V Batch {}/{}".format(i, len(validation_loader)), end='\r', flush=True)
             with torch.no_grad():
-                rgb_ins, depth_ins, past_ins, current_ins, target_ins = in_batch 
-                next_pred, aux_pred = model(rgb_ins, depth_ins, past_ins, current_ins)
-                val_loss = loss_criterion(next_pred, aux_pred, target_ins, current_ins)
+                rgb_ins, depth_ins, past_ins, target_ins = in_batch 
+                next_pred, aux_pred = model(rgb_ins, depth_ins, past_ins)
+                val_loss = loss_criterion(next_pred, aux_pred, target_ins, past_ins[:, 4])
                 val_losses.append([v.item() for v in val_loss])
 
         t_loss_mean = np.mean(train_losses, 0)

load_data.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from helper_funcs.utils import find_last
 from glob import glob
-from PIL import Image
+from scipy.spatial.transform import Rotation as R
 
 class ImageRGBDPoseHist(Dataset):
 
@@ -41,12 +41,12 @@ class ImageRGBDPoseHist(Dataset):
 
     def load_data_point(self, demo_id, img_id):
 
-        past_im_paths = self.images_by_demo[demo_id][img_id - 5: img_id]
-        current_im_path = self.images_by_demo[demo_id][img_id]
+        past_im_paths = self.images_by_demo[demo_id][img_id - 4: img_id + 1]
         next_im_path = self.images_by_demo[demo_id][img_id + 1]
 
-        depth_path = current_im_path.replace("_color_", "_depth_")
-        raw_rgb = cv2.imread(current_im_path)
+        img_path = self.images_by_demo[demo_id][img_id]
+        depth_path = img_path.replace("_color_", "_depth_")
+        raw_rgb = cv2.imread(img_path)
         raw_depth = np.expand_dims(cv2.imread(depth_path, cv2.IMREAD_GRAYSCALE) , 2)
 
         if self.rgb_trans is not None:
@@ -60,14 +60,23 @@ class ImageRGBDPoseHist(Dataset):
             img_depth = raw_depth
 
         past_poses_np = get_pose_hist(past_im_paths, self.ee_name)
-        current_pose_np = get_pose_hist([current_im_path], self.ee_name)[0]
         next_pose_np = get_pose_hist([next_im_path], self.ee_name)[0]
 
-        past_poses = torch.from_numpy(past_poses_np).to(dtype=torch.float)
-        current_pose = torch.from_numpy(current_pose_np).to(dtype=torch.float)
-        next_pose = torch.from_numpy(next_pose_np).to(dtype=torch.float)
+        # Convert to roll-pitch-yaw and normalize between -1 and 1
+        next_pose_pos, next_pose_quat = next_pose_np[0:3], next_pose_np[3:]
+        next_pose_rpy = R.from_quat(next_pose_quat).as_euler("xyz") / np.pi
+        
+        past_poses_pos, past_poses_quat = past_poses_np[:, 0:3], past_poses_np[:, 3:]
+        past_poses_rpy = R.from_quat(past_poses_quat).as_euler("xyz") / np.pi
 
-        return img_rgb, img_depth, past_poses, current_pose, next_pose
+        next_pose_pos_rpy = np.concatenate((next_pose_pos, next_pose_rpy))
+        past_poses_pos_rpy = np.concatenate((past_poses_pos, past_poses_rpy), 1)
+        
+
+        past_poses = torch.from_numpy(past_poses_pos_rpy).to(dtype=torch.float)
+        next_pose = torch.from_numpy(next_pose_pos_rpy).to(dtype=torch.float)
+
+        return img_rgb, img_depth, past_poses, next_pose
 
 def get_pose_hist(img_paths, ee_name):
     folder_paths, img_names = zip(*[split(ip) for ip in img_paths])

model.py
@@ -43,8 +43,8 @@ class ZhangNet(nn.Module):
 
         hidden_dim = 50
         image_encoding_dim = 64
-        past_hist_dim = 5 * 7
-        aux_dim = 7
+        ee_dim = 6
+        past_hist_dim = 5 * ee_dim
 
         self.ff_enc = nn.Sequential(
             nn.Linear(flattened_dims, hidden_dim),
@@ -60,18 +60,18 @@ class ZhangNet(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, aux_dim),
+            nn.Linear(hidden_dim, ee_dim),
         )
 
         self.ff_out = nn.Sequential(
-            nn.Linear(image_encoding_dim + past_hist_dim + aux_dim, hidden_dim),
+            nn.Linear(image_encoding_dim + past_hist_dim + ee_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, 7)
+            nn.Linear(hidden_dim, ee_dim)
         )
 
-    def forward(self, rgb_ims, depth_ims, past_poses, current_poses):
+    def forward(self, rgb_ims, depth_ims, past_poses):
         rgb_enc = F.relu(self.rgb_conv(rgb_ims))
         depth_enc = F.relu(self.depth_conv(depth_ims))
 
@@ -87,7 +87,7 @@ class ZhangNet(nn.Module):
         img_enc = self.ff_enc(flattened_im)
         aux_out = self.ff_aux(img_enc)
 
-        full_encoding = torch.cat((img_enc, flattened_past, current_poses), dim=1)
+        full_encoding = torch.cat((img_enc, flattened_past, aux_out), dim=1)
         output = self.ff_out(full_encoding)
         return output, aux_out
 

testMover.py
@@ -18,6 +18,7 @@ import torch
 from model import ZhangNet
 from cv_bridge import CvBridge
 import cv2
+from scipy.spatial.transform import Rotation as R
 
 def move_to_pos_rpy(group, pos, rpy):
     quat = tf.transformations.quaternion_from_euler(*rpy)
@@ -110,62 +111,65 @@ def main(model_path):
     move_to_pos_quat(r_group, right_start_pos, right_start_quat)
 
     # Open Loop: Just replicating the training run
-    # i = 0
-    # while not rospy.is_shutdown():
-    #     l_goal = train_set[i][4].numpy().astype(np.float64)
-    #     l_pos = l_goal[0:3]
-    #     l_quat = l_goal[3:]
-    #     print("pos", l_pos)
-    #     print("quat", l_quat)
-    #     print(left_start_pos)
+    i = 0
+    while not rospy.is_shutdown():
+        l_goal = train_set[i][3].numpy().astype(np.float64)
+        l_pos = l_goal[0:3]
+        l_rpy = l_goal[3:] * np.pi
 
-    #     move_to_pos_quat(l_group, l_pos, l_quat)
+        l_quat = R.from_euler(l_rpy).as_quat()
+
+        print("pos", l_pos)
+        print("rpy", l_rpy)
+        print("quat", l_quat)
+
+        # move_to_pos_quat(l_group, l_pos, l_quat)
         
-    #     if i + 5 < len(train_set) - 1:
-    #         i += 5
+        if i + 5 < len(train_set) - 1:
+            i += 5
 
-    #     r.sleep()
+        r.sleep()
 
     # Closed Loop
-    pose_history = deque(maxlen=5)
-    while not rospy.is_shutdown():
-        print("Step")
-        if not state_cache.is_empty():
-            torch_rgb = rgb_trans(state_cache.rgb_im)
-            torch_depth = depth_trans(state_cache.depth_im)
-            pose_history.append(state_cache.pose)
-            # plt.show()
-            # print("Depth im:", torch_depth.squeeze())
-            # color_path = "demos/gear_good/demo_2019_06_26_15_58_59/kinect2_qhd_image_color_rect_1561561145828572837.jpg"
-            # sanity_im = rgb_trans(cv2.imread(color_path))
+    # pose_history = deque(maxlen=5)
+    # while not rospy.is_shutdown():
+    #     print("Step")
+    #     if not state_cache.is_empty():
+    #         torch_rgb = rgb_trans(state_cache.rgb_im)
+    #         torch_depth = depth_trans(state_cache.depth_im)
+    #         pose_history.append(state_cache.pose)
+    #         # plt.show()
+    #         # print("Depth im:", torch_depth.squeeze())
+    #         # color_path = "demos/gear_good/demo_2019_06_26_15_58_59/kinect2_qhd_image_color_rect_1561561145828572837.jpg"
+    #         # sanity_im = rgb_trans(cv2.imread(color_path))
 
-            # plt.subplot(1,2, 1)
-            # plt.imshow(nn_input_to_imshow(torch_rgb))
-            # plt.subplot(1,2,2)
-            # plt.imshow(nn_input_to_imshow(sanity_im))
-            # plt.show()
+    #         # plt.subplot(1,2, 1)
+    #         # plt.imshow(nn_input_to_imshow(torch_rgb))
+    #         # plt.subplot(1,2,2)
+    #         # plt.imshow(nn_input_to_imshow(sanity_im))
+    #         # plt.show()
 
 
-            if len(pose_history) == 5:
-                # Ready to go!
-                torch_pose_hist = torch.tensor(pose_history)
-                print("Pose History: ", torch_pose_hist)
-                with torch.no_grad():
-                    current_pose = torch_pose_hist[4]
-                    current_position = current_pose[0:3].numpy().astype(np.float64)
-                    current_quat = current_pose[3:].numpy().astype(np.float64)
+    #         if len(pose_history) == 5:
+    #             # Ready to go!
+    #             torch_pose_hist = torch.tensor(pose_history)
+    #             print("Pose History: ", torch_pose_hist)
+    #             with torch.no_grad():
+    #                 current_pose = torch_pose_hist[4]
+    #                 current_position = current_pose[0:3].numpy().astype(np.float64)
+    #                 current_quat = current_pose[3:].numpy().astype(np.float64)
 
-                    print(torch_rgb)
-                    next_pose, _ = model(torch_rgb.unsqueeze(0), torch_depth.unsqueeze(0), torch_pose_hist.unsqueeze(0), torch_pose_hist[4].unsqueeze(0))
-                    next_position = next_pose[0][0:3].numpy().astype(np.float64)
-                    next_quat = next_pose[0][3:].numpy().astype(np.float64)
+    #                 print(torch_rgb)
+    #                 next_pose, _ = model(torch_rgb.unsqueeze(0), torch_depth.unsqueeze(0), torch_pose_hist.unsqueeze(0), torch_pose_hist[4].unsqueeze(0))
+    #                 next_position = next_pose[0][0:3].numpy().astype(np.float64)
+    #                 next_quat = next_pose[0][3:].numpy().astype(np.float64)
 
-                    print("Predicted Pose:", next_pose)
-                    print("Predicted Position", next_position)
-                    print("Predicted Quaternion", next_quat)
-                    move_to_pos_quat(l_group, next_position, next_quat)
-                    # move_to_pos_quat(l_group, next_position, current_quat)
-        r.sleep()
+    #                 print("Predicted Pose:", next_pose)
+    #                 print("Predicted Position", next_position)
+    #                 print("Predicted Quaternion", next_quat)
+    #                 move_to_pos_quat(l_group, next_position, next_quat)
+    #                 # move_to_pos_quat(l_group, next_position, current_quat)
+    #     r.sleep()
 
 
 